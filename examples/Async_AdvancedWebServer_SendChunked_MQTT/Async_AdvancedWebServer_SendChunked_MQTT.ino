92989937bcf672783e1069ad9a74b6e4cc53413f
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2023-01-28 22:28:06 -0500

    v1.4.2 to add examples `AsyncWebServer + MQTT`
    #### Release v1.4.2
    
    1. Add examples [Async_AdvancedWebServer_SendChunked_MQTT](https://github.com/khoih-prog/AsyncWebServer_RP2040W/tree/main/examples/Async_AdvancedWebServer_SendChunked_MQTT) and [AsyncWebServer_MQTT_RP2040W](https://github.com/khoih-prog/AsyncWebServer_RP2040W/tree/main/examples/AsyncWebServer_MQTT_RP2040W) to demo how to use `AsyncWebServer_RP2040W` and `AsyncMQTT_Generic` libraries together
    2. Improve `README.md` so that links can be used in other sites, such as `PIO`

--- examples/Async_AdvancedWebServer_SendChunked_MQTT/Async_AdvancedWebServer_SendChunked_MQTT.ino
+++ examples/Async_AdvancedWebServer_SendChunked_MQTT/Async_AdvancedWebServer_SendChunked_MQTT.ino
@@ -1,5 +1,5 @@
 /****************************************************************************************************************************
-  Async_AdvancedWebServer_MQTT_RP2040W.ino
+  Async_AdvancedWebServer_SendChunked_MQTT.ino
 
   For RP2040W with CYW43439 WiFi
 
@@ -8,21 +8,53 @@
   Based on and modified from ESPAsyncWebServer (https://github.com/me-no-dev/ESPAsyncWebServer)
   Built by Khoi Hoang https://github.com/khoih-prog/AsyncWebServer_RP2040W
   Licensed under GPLv3 license
+
+  Copyright (c) 2015, Majenko Technologies
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+  Neither the name of Majenko Technologies nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************************************************************/
+
+// See the list of country codes in
+// https://github.com/earlephilhower/cyw43-driver/blob/02533c10a018c6550e9f66f7699e21356f5e4609/src/cyw43_country.h#L59-L111
+// To modify https://github.com/earlephilhower/arduino-pico/blob/master/variants/rpipicow/picow_init.cpp
+// Check https://github.com/khoih-prog/AsyncWebServer_RP2040W/issues/3#issuecomment-1255676644
 
 #if !( defined(ARDUINO_RASPBERRY_PI_PICO_W) )
   #error For RASPBERRY_PI_PICO_W only
 #endif
 
-// Debug Level from 0 to 4
-#define _ASYNCTCP_RP2040W_LOGLEVEL_         1
-#define _ASYNC_MQTT_LOGLEVEL_               1
+#define _RP2040W_AWS_LOGLEVEL_      1
+#define _ASYNC_MQTT_LOGLEVEL_       1
 
-#if (_ASYNC_MQTT_LOGLEVEL_ > 3)
-  #warning Using RASPBERRY_PI_PICO_W with CYC43439 WiFi
-#endif
+///////////////////////////////////////////////////////////////////
 
-#include <WiFi.h>
+#include <pico/cyw43_arch.h>
+
+///////////////////////////////////////////////////////////////////
 
 #include <Ticker.h>
 
@@ -31,13 +63,10 @@
 
 // To be included only in main(), .ino with setup() to avoid `Multiple Definitions` Linker Error
 #include <AsyncMqtt_Generic.h>
-
 #include <AsyncWebServer_RP2040W.h>
 
-#include <Ticker.h>                   // https://github.com/sstaub/Ticker
-
-#define WIFI_SSID         "your_SSID"
-#define WIFI_PASSWORD     "your_PASSWORD"
+// In bytes
+#define STRING_SIZE                    40000
 
 AsyncWebServer server(80);
 AsyncMqttClient mqttClient;
@@ -47,7 +76,17 @@ AsyncMqttClient mqttClient;
 
 const char *PubTopic = "async-mqtt/RP2040W_Pub";   // Topic to publish
 
+#define WIFI_SSID         "your_SSID"
+#define WIFI_PASSWORD     "your_PASSWORD"
+
+int reqCount = 0;                // number of requests received
+
 int status = WL_IDLE_STATUS;
+
+#define LED_OFF             LOW
+#define LED_ON              HIGH
+
+////////////////////////////////////////////////////
 
 void connectToMqtt();
 void connectToMqttLoop();
@@ -61,7 +100,7 @@ bool connectedMQTT  = false;
 void printWifiStatus()
 {
   // print the SSID of the network you're attached to:
-  Serial.print("Connected to SSID: ");
+  Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
   // print your board's IP address:
@@ -69,11 +108,16 @@ void printWifiStatus()
   Serial.print("Local IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("Signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  // print your board's country code
+  // #define CYW43_COUNTRY(A, B, REV) ((unsigned char)(A) | ((unsigned char)(B) << 8) | ((REV) << 16))
+  uint32_t myCountryCode = cyw43_arch_get_country_code();
+  char countryCode[3] = { 0, 0, 0 };
+
+  countryCode[0] = myCountryCode & 0xFF;
+  countryCode[1] = (myCountryCode >> 8) & 0xFF;
+
+  Serial.print("Country code: ");
+  Serial.println(countryCode);
 }
 
 bool connectToWifi()
@@ -263,16 +307,136 @@ void onMqttPublish(const uint16_t& packetId)
   Serial.println(packetId);
 }
 
+///////////////////////////////////////////////////
+
+#define BUFFER_SIZE         512
+char temp[BUFFER_SIZE];
+
+void handleRoot(AsyncWebServerRequest *request)
+{
+  static uint32_t pageCount   = 0;
+  static uint32_t maxfreeHeap = 0;
+  static uint32_t minFreeHeap = 0xFFFFFFFF;
+  uint32_t curFreeHeap = rp2040.getFreeHeap();
+
+  if (maxfreeHeap < curFreeHeap)
+    maxfreeHeap = curFreeHeap;
+
+  if (minFreeHeap > curFreeHeap)
+    minFreeHeap = curFreeHeap;
+
+  digitalWrite(LED_BUILTIN, LED_ON);
+
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+  int day = hr / 24;
+
+  snprintf(temp, BUFFER_SIZE - 1,
+           "<html>\
+<head>\
+<meta http-equiv='refresh' content='5'/>\
+<title>AsyncWebServer-%s</title>\
+<style>\
+body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+</style>\
+</head>\
+<body>\
+<h2>AsyncWebServer_SendChunked_MQTT!</h2>\
+<h3>running WiFi on %s</h3>\
+<p>Uptime: %d d %02d:%02d:%02d, pageCount: %lu</p>\
+<p>Heap Free: %lu, Max: %lu, Min: %lu</p>\
+<img src=\"/test.svg\" />\
+</body>\
+</html>", BOARD_NAME, BOARD_NAME, day, hr % 24, min % 60, sec % 60, ++pageCount, curFreeHeap, maxfreeHeap, minFreeHeap);
+
+  request->send(200, "text/html", temp);
+
+  digitalWrite(LED_BUILTIN, LED_OFF);
+}
+
+void handleNotFound(AsyncWebServerRequest *request)
+{
+  digitalWrite(LED_BUILTIN, LED_ON);
+  String message = "File Not Found\n\n";
+
+  message += "URI: ";
+  message += request->url();
+  message += "\nMethod: ";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += request->args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < request->args(); i++)
+  {
+    message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+  }
+
+  request->send(404, "text/plain", message);
+  digitalWrite(LED_BUILTIN, LED_OFF);
+}
+
+String out;
+
+void drawGraph(AsyncWebServerRequest *request)
+{
+  out.reserve(STRING_SIZE);
+  char temp[70];
+
+  out = String();
+
+  digitalWrite(LED_BUILTIN, LED_ON);
+
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"1810\" height=\"150\">\n";
+  out += "<rect width=\"1810\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"2\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"blue\">\n";
+  int y = rand() % 130;
+
+  for (int x = 10; x < 5000; x += 10)
+  {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"2\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+
+  out += "</g>\n</svg>\n";
+
+  AWS_LOGDEBUG1("Total length to send in chunks =", out.length());
+
+  AsyncWebServerResponse *response = request->beginChunkedResponse("image/svg+xml", [](uint8_t *buffer, size_t maxLen, size_t filledLength) -> size_t
+  {
+    size_t len = min(maxLen, out.length() - filledLength);
+    memcpy(buffer, out.c_str() + filledLength, len);
+
+    AWS_LOGDEBUG1("Bytes sent in chunk =", len);
+    
+    return len;
+  });
+
+  request->send(response);
+
+  digitalWrite(LED_BUILTIN, LED_OFF);
+}
+
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LED_OFF);
+
   Serial.begin(115200);
 
   while (!Serial && millis() < 5000);
 
   delay(200);
 
-  Serial.print("\nStart Async_AdvancedWebServer_MQTT_RP2040W on ");
-  Serial.println(BOARD_NAME);
+  Serial.print("\nStart Async_AdvancedWebServer_SendChunked_MQTT on ");
+  Serial.print(BOARD_NAME);
+  Serial.print(" with ");
+  Serial.println(SHIELD_TYPE);
+  Serial.println(ASYNCTCP_RP2040W_VERSION);
+  Serial.println(ASYNC_WEBSERVER_RP2040W_VERSION);
   Serial.println(ASYNC_MQTT_GENERIC_VERSION);
 
   ///////////////////////////////////
@@ -293,20 +457,37 @@ void setup()
   connectToMqttTicker.start(); //start the ticker.
 
   connectToMqtt();
+  
+  ///////////////////////////////////
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
   {
-    request->send(200, "text/plain", "Hello from RP2040W!");
+    handleRoot(request);
   });
 
+  server.on("/test.svg", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    drawGraph(request);
+  });
+
+  server.on("/inline", [](AsyncWebServerRequest * request)
+  {
+    request->send(200, "text/plain", "This works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
   server.begin();
+
+  Serial.print(F("AsyncWebServer is @ IP : "));
+  Serial.println(WiFi.localIP());
 }
 
 void heartBeatPrint()
 {
   static int num = 1;
 
-  Serial.print(F("H"));
+  Serial.print(F("."));
 
   if (num == 80)
   {
@@ -326,7 +507,7 @@ void check_status()
 #define STATUS_CHECK_INTERVAL     10000L
 
   // Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
-  if ((millis() > checkstatus_timeout) /*|| (checkstatus_timeout == 0)*/)
+  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
   {
     heartBeatPrint();
     checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
@@ -335,6 +516,5 @@ void check_status()
 
 void loop()
 {
-  connectToMqttTicker.update(); //update the ticker.
   check_status();
 }
